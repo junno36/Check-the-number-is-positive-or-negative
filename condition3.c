67310a24e6d559acad79a1cb101539263b0e8965
junno36 <108143721+junno36@users.noreply.github.com>
2022-07-31 13:46:20 +0600

    Add files via upload

---
+++ condition3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,res;
+    scanf("%d",&a);
+    if (a > 0)
+    {
+        printf("%d is a positve number.", a);
+    }
+    else
+    {
+        printf("%d is a negative number.", a);
+    }
+
+    return 0;
+}
